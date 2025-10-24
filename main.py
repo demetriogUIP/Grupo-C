5e627b74173c54456ad0d7c20bf7e33eddf24cea
demetriogUIP <demetrio.garcia@uip.edu.pa>
2025-10-24 16:45:26 -0500

    refactor: Ajustes y pruebas en main.py

--- main.py
+++ main.py
@@ -16,7 +16,6 @@ print(f"La suma de {num1} y {num2} es: {resultado_suma}")
 resultado_resta = operaciones.restar(num1, num2)
 print(f"La resta de {num1} y {num2} es: {resultado_resta}")
 
-# Nuevas funciones
 resultado_multiplicacion = operaciones.multiplicar(num1, num2)
 print(f"La multiplicacion de {num1} y {num2} es: {resultado_multiplicacion}")
 
